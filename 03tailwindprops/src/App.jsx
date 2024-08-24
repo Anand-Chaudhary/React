185314fcbad5ceeed94f2d40609e95652b0006f0
Aakash <chaudharyaakash234@gmail.com>
2024-08-24 22:17:38 +0545

    Tailwind w/ React

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -5,13 +5,18 @@ import './App.css'
 import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const myObj = {
+    username : "Aakash",
+    age : 21
+  };
+  let arr = [1, 2, 3];
 
   return (
     <>
     <h1 className='p-4 bg-white text-black rounded-xl mb-5'>Using Tailwind</h1>
-    <Card channels = "Chai aur Code"/>
-    <Card />
+    <Card username = "Chai aur Code" btnText = "Click me please"/>
+    <Card username= "Aakash"/>
     </>
   )
 }
