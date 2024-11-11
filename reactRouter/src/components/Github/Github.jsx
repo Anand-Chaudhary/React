3007a2d74971191d805c77ab72d411287a8ade90
Aakash <chaudharyaakash234@gmail.com>
2024-11-11 22:32:12 +0545

    React Router

--- reactRouter/src/components/Github/Github.jsx
+++ reactRouter/src/components/Github/Github.jsx
@@ -7,10 +7,14 @@ function Github() {
         .then(response => response.json())
         .then(data => {
             console.log(data);
-        })
+            setData(data);
+        });
     }, [])
   return (
-    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl tracking-tighter'>Github: {data.followers}</div>
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl tracking-tighter'>
+      Github: {data.followers}
+      <img src={data.avatar_url} alt="Profile Picture" width={300} />
+    </div>
   )
 }
 
