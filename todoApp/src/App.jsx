19094791f820f9c2193b6b7a19cfbf94d1e87f57
Aakash <chaudharyaakash234@gmail.com>
2024-11-15 22:52:52 +0545

    To Do App

--- todoApp/src/App.jsx
+++ todoApp/src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
 import './App.css'
+import { ToDoProvider } from './context'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState([])
+  const addTodo = (todo) =>{
+    setTodo((prev) => [...prev, {id: Date.now, ...todo}]);
+  }
 
   return (
-    <>
-      <h1 className='font-sans underline text-3xl text-white tracking-tighter'>Manage Todos</h1>
-    </>
+    <ToDoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+      <div className="min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-3xl font-sans underline text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </ ToDoProvider>
   )
 }
 
